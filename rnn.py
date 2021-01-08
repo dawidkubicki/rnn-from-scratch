c9139056f880706db305d2b7f1060c1a63cb1391
Dawid Kubicki <dawid@pluscode.io>
2021-01-08 12:57:37 +0100

    forward cells

--- rnn.py
+++ rnn.py
@@ -14,3 +14,23 @@ def rnn_cell_forward(xt, a_prev, parameters):
 	cache = (a_next, a_prev, xt, parameters)
 
 	return a_next, yt_pred, cache
+
+def rnn_forward_pass(x, a0, parameters):
+	caches = []
+	
+	_, m, T_x = x.shape
+	n_y, n_a = parameters["Wya"].shape
+
+	a = np.zeros((n_a, m, T_x))
+
+	a_next = a0
+
+	for t in range (T_x):
+		a_next, yt_pred, cache = rnn_cell_forward(x[:,:,t], a_next, parameters)
+		a[:,:,t] = a_next
+		y_pred[:,:,t] = a_next
+	
+	caches.append(cache)
+	caches = (caches, x)
+
+	return a, y_pred, caches
